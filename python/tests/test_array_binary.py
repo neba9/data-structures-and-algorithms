bc5ff7a341cd6514ecc5c675a0419187beb3ab7a
nebiyu kifle <rema9.ne@gmail.com>
2020-12-15 14:41:14 -0800

    addressed code-challenge

--- python/tests/test_array_binary.py
+++ python/tests/test_array_binary.py
@@ -1,22 +1,15 @@
 # import pytest
-from array_binary_search import __version__
+from array_binary_search.array_binary_search  import binary_search
 
+def test_binary_search():
+  
+    arr = [4,8,15,16,23,42]
+    x = 15
 
-def test_version():
-    assert __version__=='0.1.0'
-arr = [4,8,15,16,23,42]
-x = 10
-def test_binary_search(arr, x):
-  if test_binary_search != -1:
-    return 
-    actual = test_binary_search
-    expec
+    expected = 2
+    actual = binary_search(arr,x)
+    assert expected == actual
 
  
 
 
-# from array_binary_search import array_binary_search
-
-
-# def test_import():
-#     assert array_binary_search
